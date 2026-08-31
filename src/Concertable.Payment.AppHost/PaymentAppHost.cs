7e4c38ed67ea0de7fd167c126972ad20cc943ad5
ThomasSeery <tommyboy4050@outlook.com>
2026-08-31 02:33:52 +0100

    refactor-apphost-hosting-package-cutover

--- src/Concertable.Payment.AppHost/PaymentAppHost.cs
+++ src/Concertable.Payment.AppHost/PaymentAppHost.cs
@@ -4,6 +4,9 @@ using Concertable.Payment.Hosting;
 
 public static class PaymentAppHost
 {
+    private const string AuthImage = "ghcr.io/concertable/auth";
+    private const string AuthDigest = "sha256:8b7ba47efb319e6e1f1b5b86223d4075b9c8e09920933dae24fbf35f72851a63";
+
     public static IDistributedApplicationBuilder CreateBuilder(string[] args)
     {
         var builder = StrictDistributedApplication.CreateBuilder(args);
@@ -12,7 +15,7 @@ public static class PaymentAppHost
         var paymentDb = sql.AddDatabase(PaymentConstants.Database);
         var asb = builder.AddServiceBus();
         asb.Topology().AddPaymentTopology().AddAuthTopology().RunAsEmulator();
-        var auth = builder.AddAuth<Projects.Concertable_Auth>(authDb, asb);
+        var auth = builder.AddAuth(AuthImage, AuthDigest, authDb, asb);
         auth.WithEnvironment("ServiceAuth__AuthClientId", "concertable-auth");
         var paymentWeb = builder.AddPaymentWeb<Projects.Concertable_Payment_Web>(auth, paymentDb, asb);
         builder.AddPaymentWorkers<Projects.Concertable_Payment_Workers>(paymentDb, asb);
